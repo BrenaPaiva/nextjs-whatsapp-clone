7bd57d249d52ab96c7f7157ee17a2f5ce4221456
Brena <bpaiva@uniaoquimica.com.br>
2023-07-27 21:39:52 -0300

    Server started

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Image from "next/image";
 
 function login() {
-  return <div>login</div>;
+  return <div className="flex justify-center items-center bg-panel-header-background h-screen flex-col gap-6 ">
+    <div className="flex items-center justify-center gap-2 text-white">
+      <Image src="/whatsapp.gif" alt="whatsapp" height={300} width={300} />
+      <span>Whatsapp</span>
+
+    </div>
+  </div>;
 }
 
 export default login;
